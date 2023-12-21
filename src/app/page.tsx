6fcb036acc0cc107e1f396717c570b51c97c8779
maikb <maik.basel@gmx.de>
2023-12-21 19:46:41 +0100

    refactor(frontend): Import React in Home and Configuration components
    The Home and Configuration pages in the application and profiles
    directories, respectively, have been updated with React imports. This
     is a necessary update for these components to use React
     functionalities.
    
    Refs: #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Home() {
   return (
     <div className='flex h-full flex-col pb-20 pt-4 sm:ml-[120px] sm:border-r sm:border-zinc-700 md:ml-[250px]'>
--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 export default function Configuration() {
   return (
     <div className='flex min-h-screen flex-col pb-20 pt-4 sm:ml-[120px] sm:border-r sm:border-zinc-700 md:ml-[250px]'>
