ff3cb0a3f76abdfc55594da7ed4ec6fd6ac5d297
maikb <maik.basel@gmx.de>
2023-12-22 22:49:58 +0100

    test(backend): Add tests for extract_printable_attachments function
    Tests for the `extract_printable_attachments` function in `report.rs`
     have been added. These tests check that the function correctly
     extracts printable attachments from a report, as well as handles
     cases where there are no printable attachments or only non-printable
      ones.
    
    Refs: #1

--- src-tauri/src/common/report.rs
+++ src-tauri/src/common/report.rs
@@ -1,5 +1,6 @@
-use crate::profile::core::error::ProfileError;
 use error_stack::{AttachmentKind, FrameKind, Report};
+
+use crate::profile::core::error::ProfileError;
 
 pub fn extract_printable_attachments(report: &Report<ProfileError>) -> Vec<String> {
     report
@@ -12,4 +13,34 @@ pub fn extract_printable_attachments(report: &Report<ProfileError>) -> Vec<Strin
             }
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::profile::core::error::ProfileError;
+
+    use super::*;
+
+    #[test]
+    fn should_return_empty_printable_attachments_given_no_printable_attachments_are_attached() {
+        let report = Report::new(ProfileError::InvalidProfileNameError);
+
+        assert_eq!(extract_printable_attachments(&report), Vec::<String>::new());
+    }
+
+    #[test]
+    fn should_return_empty_printable_attachments_given_only_non_printable_attachments_are_attached() {
+        let report = Report::new(ProfileError::InvalidProfileNameError)
+            .attach("non printable");
+
+        assert_eq!(extract_printable_attachments(&report), Vec::<String>::new());
+    }
+
+    #[test]
+    fn should_extract_printable_attachments() {
+        let report = Report::new(ProfileError::InvalidProfileNameError)
+            .attach_printable("printable attachment");
+
+        assert_eq!(extract_printable_attachments(&report), vec!["printable attachment".to_string()]);
+    }
 }
