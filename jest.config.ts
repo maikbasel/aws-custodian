31b52bbe79ac4e394b98b4bbd379c70adcf25fb0
maikb <maik.basel@gmx.de>
2024-01-13 13:22:43 +0100

    ci(frontend): Add 'jest-junit' to Jest config and simplify 'test:ci' script
    The 'jest-junit' reporter has been added directly to the Jest config,
     removing the need to specify it in the 'test:ci' script. This change
      simplifies the 'test:ci' script and ensures the reporter is used in
       all testing scenarios.

--- jest.config.ts
+++ jest.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  reporters: ['default', 'jest-junit'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
