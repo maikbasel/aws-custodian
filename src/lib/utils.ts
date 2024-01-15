b7c38bd3d75252e3d28cf7085bb96569e9faec21
maikb <maik.basel@gmx.de>
2024-01-15 21:53:10 +0100

    docs(frontend): Update comments for 'cn' function
    Improved the 'cn' function's documentation in the 'utils.ts' file to
    provide clearer understanding of its use and parameters.
    
    Refs: #1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines multiple class names or class arrays into a single string of class names.
+ *
+ * @param {...ClassValue[]} inputs - The class names or class arrays to be combined.
+ * @return {string} - A string containing the combined class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
