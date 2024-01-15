d425e7f005df7fb67771a953208366da15edc610
maikb <maik.basel@gmx.de>
2024-01-15 22:14:16 +0100

    test(frontend): Update SideNav tests, remove child items test
    This commit refactors the SideNav component tests by removing the
    child items test (as they don't work) and adding a new feature to
    test the current path matching with the navigation item's href. The
    test checks if the matched navigation item has distinguishing visual
    characteristics, thus maintaining code hygiene and efficiency.
    
    Refs: #1

--- src/sections/dashboard/components/sidebar/side-nav.test.tsx
+++ src/sections/dashboard/components/sidebar/side-nav.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CatIcon } from 'lucide-react';
 import {
   NavItem,
   SideNav,
 } from '@/sections/dashboard/components/sidebar/side-nav';
+
+const mockUsePathname = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  usePathname() {
+    return mockUsePathname();
+  },
+}));
 
 describe('<SideNav />', function () {
   const items: NavItem[] = [
@@ -16,6 +24,10 @@ describe('<SideNav />', function () {
       color: 'red',
     },
   ];
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('should render with passed nav items', () => {
     render(<SideNav items={items} setOpen={jest.fn()} className='testClass' />);
@@ -36,71 +48,14 @@ describe('<SideNav />', function () {
     expect(mockSetOpen).toHaveBeenCalled();
   });
 
-  it('should render parent item with child items when hasChildren is true', () => {
-    const nestedItems: NavItem[] = [
-      {
-        title: 'Test Parent Item',
-        href: '/parent',
-        icon: CatIcon,
-        color: 'blue',
-        hasChildren: true,
-        children: [
-          {
-            hasChildren: false,
-            title: 'Test Child Item',
-            href: '/child',
-            icon: CatIcon,
-            color: 'red',
-          },
-        ],
-      },
-    ];
-    render(
-      <SideNav items={nestedItems} setOpen={jest.fn()} className='testClass' />
-    );
-
-    const parentItem = screen.getByText(/Test Parent Item/i);
-    expect(parentItem).toBeInTheDocument();
-
-    fireEvent.click(parentItem);
-
-    const childItem = screen.getByText(/Test Child Item/i);
-    expect(childItem).toBeInTheDocument();
-  });
-
-  it('should trigger setOpen on nested nav item click', () => {
+  it('should display muted title text when current path equals nav item href', () => {
+    mockUsePathname.mockReturnValue('/test');
     const mockSetOpen = jest.fn();
-    const nestedItems: NavItem[] = [
-      {
-        title: 'Test Parent Item',
-        href: '/parent',
-        icon: CatIcon,
-        color: 'blue',
-        hasChildren: true,
-        children: [
-          {
-            hasChildren: false,
-            title: 'Test Child Item',
-            href: '/child',
-            icon: CatIcon,
-            color: 'red',
-          },
-        ],
-      },
-    ];
     render(
-      <SideNav
-        items={nestedItems}
-        setOpen={mockSetOpen}
-        className='testClass'
-      />
+      <SideNav items={items} setOpen={mockSetOpen} className='testClass' />
     );
+    const anchor = screen.getByRole('link', { name: /Test Item/i });
 
-    const parentItem = screen.getByText(/Test Parent Item/i);
-    fireEvent.click(parentItem);
-    const childItem = screen.getByText(/Test Child Item/i);
-    fireEvent.click(childItem);
-
-    expect(mockSetOpen).toHaveBeenCalledTimes(1);
+    expect(anchor).toHaveClass('bg-muted', 'font-bold', 'hover:bg-muted');
   });
 });
