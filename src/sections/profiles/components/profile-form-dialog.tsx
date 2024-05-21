99cfa81c384decd9fff4e9be75280f66f4be495f
Maik <maik.basel@gmx.de>
2024-05-21 16:10:30 +0200

    refactor(frontend): Remove debug logs

--- src/sections/profiles/components/profile-form-dialog.tsx
+++ src/sections/profiles/components/profile-form-dialog.tsx
@@ -466,8 +466,6 @@ export default function ProfileFormDialog({
   const { resetFormData } = userProfileFormStore();
 
   function onOpenChange(openState: boolean) {
-    console.info('is edit', profile != undefined);
-    console.info('open state changed', openState);
     setOpen(openState);
     if (open) {
       resetFormData();
