e151cb3657ea4bf550f84728b29508b247e1b721
Maik <maik.basel@gmx.de>
2024-05-22 17:28:28 +0200

    fix(frontend): Init CredentialsDataAdapter in DI context
    Refs: #9 #15 #16

--- src/context/di-context.tsx
+++ src/context/di-context.tsx
@@ -3,17 +3,22 @@
 import { ProfileDataSPI } from '@/modules/profiles/core/domain';
 import React, { createContext, useMemo } from 'react';
 import { createProfileDataAdapter } from '@/modules/profiles/infrastructure/profile-data-adapter';
+import { createCredentialsDataAdapter } from '@/modules/credentials/infrastructure/credentials-data-adapter';
+import { CredentialsDataSPI } from '@/modules/credentials/core/domain';
 
 type DIContextState = {
   profileDataSPI: ProfileDataSPI;
+  credentialsDataSPI: CredentialsDataSPI;
 };
 
 export const DIContext = createContext<DIContextState>({} as DIContextState);
 
 export const DIContextProvider = ({ children }: React.PropsWithChildren) => {
   const profileDataSPI = useMemo(() => createProfileDataAdapter(), []);
+  const credentialsDataSPI = useMemo(() => createCredentialsDataAdapter(), []);
+
   return (
-    <DIContext.Provider value={{ profileDataSPI }}>
+    <DIContext.Provider value={{ profileDataSPI, credentialsDataSPI }}>
       {children}
     </DIContext.Provider>
   );
