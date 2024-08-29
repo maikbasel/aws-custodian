8eb9c7d673e430f857a759b0ffafb4db3abe1a4f
maikb <maik.basel@gmx.de>
2024-08-29 21:10:04 +0200

    refactor(backend): Configure ssm retries
    Refs: #2

--- src-tauri/src/common/aws.rs
+++ src-tauri/src/common/aws.rs
@@ -1,6 +1,8 @@
+use std::time::Duration;
 use aws_config::{BehaviorVersion, ConfigLoader};
 use aws_sdk_ssm::config::Builder as SsmBuilder;
 use aws_sdk_ssm::Client as SsmClient;
+use aws_sdk_ssm::config::retry::RetryConfig;
 use aws_sdk_sts::config::Builder as StsBuilder;
 use aws_sdk_sts::Client as StsClient;
 
@@ -27,8 +29,11 @@ pub fn sts_client(config: &aws_config::SdkConfig) -> StsClient {
 }
 
 pub fn ssm_client(config: &aws_config::SdkConfig) -> SsmClient {
-    // Copy config from aws_config::SdkConfig to aws_sdk_sts::Config
-    let sts_config_builder = SsmBuilder::from(config);
+    let retry_config = RetryConfig::adaptive()
+        .with_max_attempts(8)
+        .with_initial_backoff(Duration::from_millis(200))
+        .with_max_backoff(Duration::from_secs(5));
+    let sts_config_builder = SsmBuilder::from(config).retry_config(retry_config);
 
     SsmClient::from_conf(sts_config_builder.build())
 }
